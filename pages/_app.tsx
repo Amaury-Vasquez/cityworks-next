79d76d86dafd5f90b68266cd33d8f3fb9b919c5a
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-06-18 17:30:40 -0600

    feat: add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.scss';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MainLayout } from '@/layout';
 import { Montserrat } from 'next/font/google';
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './modules/**/*.{js,jsx,ts,tsx}',
+    './layouts/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#212125',
+        white: '#e2ffff',
+        primary: '#f7f39a',
+        secondary: '#b8b578',
+        darker: '#080400',
+        gray: '#414141',
+      },
+    },
+  },
+  plugins: [],
+};
